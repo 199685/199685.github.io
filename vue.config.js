e981f90f8cfc67ad2d9fafa9380ccee27bdb17b7
chi <jfhfjj441@gmail.com>
2021-01-07 11:23:18 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,8 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/199685.github.io/'
     : '/',
+  rules: {
+      'linebreak-style': ["error", "windows"],
+      "global-require": 0  
+  }
 };
